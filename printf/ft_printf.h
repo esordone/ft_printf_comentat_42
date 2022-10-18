48442b98ba197b44b38ccba5fc831fda51502217
estrella-estelar <110181623+estrella-estelar@users.noreply.github.com>
2022-10-18 15:07:47 +0200

    Add files via upload

---
+++ printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esordone <esordone@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 14:50:38 by esordone          #+#    #+#             */
+/*   Updated: 2022/10/18 15:04:22 by esordone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_printf(char const *format, ...);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+#endif
